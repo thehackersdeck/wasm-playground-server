24ac274072696dbba74bf4deaadd19d2f8a5c9e4
Somtozech <ezechinnaemeka@gmail.com>
2018-12-03 22:21:40 +0100

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
-import path from 'path';
-import express from 'express';
-import bodyParser from 'body-parser';
-import router from './routes/router';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import config from '../webpack.dev';
-import zip from 'express-zip';
+import path from "path";
+import express from "express";
+import router from "./routes/router";
+import cors from "cors";
+import dotenv from "dotenv";
+import webpack from "webpack";
+import webpackDevMiddleware from "webpack-dev-middleware";
+import webpackHotMiddleware from "webpack-hot-middleware";
+import config from "../webpack.dev";
+import zip from "express-zip";
 
 // Create express app
 const app = express();
@@ -20,25 +19,29 @@ const compiler = webpack(config);
 dotenv.config();
 
 // Enable cors
-app.use(cors('*'));
+app.use(cors("*"));
 
 // DEVONLY: Set up dev-middleware
-app.use(webpackDevMiddleware(compiler, {
+app.use(
+  webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath
-}));
+  })
+);
 
 // DEVONLY: Set up HMR
 app.use(webpackHotMiddleware(compiler));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Public directory for static files
-app.use('/', express.static(path.join(__dirname, '../client')));
+app.use("/", express.static(path.join(__dirname, "../client")));
 
 // Router
-app.use('/', router);
+app.use("/", router);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => { console.log(`Listening on port : ${PORT}`); });
+app.listen(PORT, () => {
+  console.log(`Listening on port : ${PORT}`);
+});
